44997d385eda9924684eaa1914e1f2e8fd8063aa
h662 <h662hong@gmail.com>
2023-11-26 18:23:48 +0900

    Detail Layout

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import englishData from "../englishData.json";
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
-  return <div>Detail</div>;
+  const [dailyData, setDailyData] = useState();
+
+  const { day } = useParams();
+
+  useEffect(() => {
+    englishData.map((v, i) => {
+      if (v.day === +day) {
+        setDailyData(v);
+      }
+    });
+  }, []);
+  useEffect(() => console.log(dailyData), [dailyData]);
+
+  return dailyData ? (
+    <div className="min-h-screen max-w-screen-md mx-auto px-8 mt-20">
+      <h1 className="text-center text-2xl font-semibold">
+        Day {dailyData.day} - {dailyData.title}
+      </h1>
+      <div className="mt-12">
+        <div>{dailyData.sentences[0].english}</div>
+        <div className="mt-2">{dailyData.sentences[0].korean}</div>
+        <div className="mt-4 flex gap-2">
+          <button className="border-2 border-black px-2 rounded-md">
+            Prev
+          </button>
+          <button className="border-2 border-black px-2 rounded-md">
+            Next
+          </button>
+          <button className="border-2 border-black px-2 rounded-md">
+            Sound
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 export default Detail;
